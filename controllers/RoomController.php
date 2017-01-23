9bfdb154c1f6b9d8659f3b18540cad0ed9cf114f
dmitriy <dmitriy>
2017-01-23 23:37:51 +0300

    Фикс вывода вопросов для пользователей

--- controllers/RoomController.php
+++ controllers/RoomController.php
@@ -47,8 +47,6 @@ class RoomController extends Controller
         // check for admin permission (`tbl_role.can_admin`)
         // note: check for Yii::$app->user first because it doesn't exist in console commands (throws exception)
         if (!empty(Yii::$app->user) && !Yii::$app->user->can("admin")) {
-            throw new ForbiddenHttpException('You are not allowed to perform this action.');
-        } else {
             $this->redirect('/area/');
         }
 
