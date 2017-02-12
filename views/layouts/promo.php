844d86ecc5a6cac9476c60ad55d9edc10e5e24ca
dmitriy <dmitriy>
2017-02-12 22:02:51 +0300

    Фикс лишнего текста

--- views/layouts/promo.php
+++ views/layouts/promo.php
@@ -43,7 +43,6 @@
 </head>
 
 <body id="page-top">
-<?php $this->beginBody() ?>
 
 <nav id="mainNav" class="navbar navbar-default navbar-fixed-top affix">
     <div class="container-fluid">
@@ -98,7 +97,7 @@
 </section>
 <!-- Yandex.Metrika counter --> <script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter42253589 = new Ya.Metrika({ id:42253589, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, trackHash:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/42253589" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
 
-<?php $this->endBody() ?>
+
 
 <script>
     $(function () {
